379bdd2fe6ec343c709972a93afcf0a6664e380e
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-01 00:29:06 +0100

    dodanie importów i deklaracji

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { MessageComponent } from './messages/message/message.component';
+import {MessageComponent} from './messages/message/message.component';
 import {HttpService} from "./services/http.service";
+import {FormsModule} from "@angular/forms";
+import {UserComponent} from './messages/user/user.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessageComponent
+    MessageComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
